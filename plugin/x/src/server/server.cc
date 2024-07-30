80a04cd9e876d37a6579a00c2437b7a30012fc29
Dmitry Lenev <dmitry.lenev@percona.com>
2024-07-30 16:17:48 +0200

    PS-7125 feature: Reloading X ssl certificates on ALTER INSTANCE RELOAD TLS (To revert during merge)
    https://jira.percona.com/browse/PS-7125
    
    Issue: MySQL lets users change certificates while the server is running,
    but this change isn't propagated to the X plugin. X connections still
    keep using the old certificate.
    
    Fix: this commit adds a callback to the plugin interface, notifying
    plugins if the user executed ALTER INSTANCE RELOAD TLS. The X plugin
    implements this callback, and reloads certificates correctly. Existing
    connections also keep using the previous certificate, similarly to
    normal connections.
    
    -----------------------------------------------------
    
    Weak ciphers usage deprecated starting from 8.2.0 in
    https://github.com/mysql/mysql-server/commit/5ec9f47df610175f95cee9de51d9405c25209cb7.
    Changed DHE-RSA-AES256-SHA to ECDHE-RSA-AES256-GCM-SHA384.
    
    -----------------------------------------------------
    
    PS-9218: Merge MySQL 8.4.0 (fix ssl context reloading for X plugin)
    
    https://perconadev.atlassian.net/browse/PS-9218

--- plugin/x/src/server/server.cc
+++ plugin/x/src/server/server.cc
@@ -56,6 +56,25 @@
 #include "my_systime.h"  // my_sleep() NOLINT(build/include_subdir)
 
 namespace ngs {
+namespace {
+
+std::unique_ptr<xpl::Sql_data_context> build_sql_data_context() noexcept {
+  auto ctx = std::make_unique<xpl::Sql_data_context>();
+  const bool admin_session = true;
+  ngs::Error_code error = ctx->init(admin_session);
+
+  if (error) {
+    log_error(ER_XPLUGIN_STARTUP_FAILED, error.message.c_str());
+    return nullptr;
+  }
+
+  ctx->switch_to_local_user(MYSQL_SESSION_USER);
+  ctx->attach();
+
+  return ctx;
+}
+
+}  // namespace
 
 Server::Server(std::shared_ptr<Scheduler_dynamic> accept_scheduler,
                std::shared_ptr<Scheduler_dynamic> work_scheduler,
@@ -116,21 +135,16 @@ void Server::delayed_start_tasks() {
             [this]() { return is_terminating(); })) {
       SYNC_POINT_CHECK("xplugin_init_wait");
 
-      xpl::Sql_data_context sql_context;
-      const bool admin_session = true;
-      ngs::Error_code error = sql_context.init(admin_session);
+      auto sql_context = build_sql_data_context();
 
-      if (error) {
-        log_error(ER_XPLUGIN_STARTUP_FAILED, error.message.c_str());
+      if (sql_context == nullptr) {
         return;
       }
-      sql_context.switch_to_local_user(MYSQL_SESSION_USER);
-      sql_context.attach();
 
       /* This method is executed inside a worker thread, thus
          its better not to swap another thread, this one can handle
          a task. */
-      start_tasks(&sql_context);
+      start_tasks(sql_context.get());
     }
   });
 }
