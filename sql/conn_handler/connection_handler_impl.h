mydb-ru/mydb-server

PS-9614 Pool-of-Threads' timer thread fails to start if mysqld is started with --daemonize
Problem:
When pool-of-threads is enabled, a timer_thread should be created. However, if the process starts with --daemonize, the thread is missing.
This occurs because during the execution of mysqld_daemonize(), the mysqld process forks, creating a child process that runs as a daemon.
Since threads are not copied during a fork, and timer_thread is not reinitialized in the daemon process, it is lost.

Solution:
Move the timer_thread initialization to execute after the mysqld_daemonize() function.

sql/conn_handler/connection_handler_impl.h
@@ -134,6 +134,8 @@ class Thread_pool_connection_handler : public Connection_handler {
   bool add_connection(Channel_info *channel_info) override;
 
   uint get_max_threads() const override { return threadpool_max_threads; }
+
+  void post_daemonize_init() override { start_timer_thread(); }
 };
 
 #endif  // CONNECTION_HANDLER_IMPL_INCLUDED

sql/threadpool.h
@@ -80,4 +80,7 @@ extern void tp_set_max_threads(uint val);
 extern void tp_set_threadpool_size(uint val) noexcept;
 extern void tp_set_threadpool_stall_limit(uint val) noexcept;
 
+/* Function to start the timer thread */
+extern void start_timer_thread() noexcept;
+
 #endif /* THREADPOOL_INCLUDED */

sql/threadpool_unix.cc
@@ -59,6 +59,8 @@ const char *threadpool_high_prio_mode_names[] = {"transactions", "statements",
 
 /** Indicates that threadpool was initialized*/
 static bool threadpool_started = false;
+/** Indicates that timer thread was started*/
+static bool timer_thread_started = false;
 
 /*
   Define PSI Keys for performance schema.
@@ -602,6 +604,7 @@ static void check_stall(thread_group_t *thread_group) {
 static void start_timer(pool_timer_t *timer) noexcept {
   my_thread_handle thread_id;
   DBUG_ENTER("start_timer");
+  timer_thread_started = true;
   mysql_mutex_init(key_timer_mutex, &timer->mutex, NULL);
   mysql_cond_init(key_timer_cond, &timer->cond);
   timer->shutdown = false;
@@ -609,10 +612,16 @@ static void start_timer(pool_timer_t *timer) noexcept {
   DBUG_VOID_RETURN;
 }
 
+void start_timer_thread() noexcept {
+  pool_timer.tick_interval = threadpool_stall_limit;
+  start_timer(&pool_timer);
+}
+
 static void stop_timer(pool_timer_t *timer) noexcept {
   DBUG_ENTER("stop_timer");
   mysql_mutex_lock(&timer->mutex);
   timer->shutdown = true;
+  timer_thread_started = false;
   mysql_cond_signal(&timer->cond);
   mysql_mutex_unlock(&timer->mutex);
   DBUG_VOID_RETURN;
@@ -1488,8 +1497,6 @@ bool tp_init() {
   mysql_thread_register("threadpool", thread_list, array_elements(thread_list));
 #endif
 
-  pool_timer.tick_interval = threadpool_stall_limit;
-  start_timer(&pool_timer);
   DBUG_RETURN(false);
 }
 
@@ -1497,8 +1504,7 @@ void tp_end() noexcept {
   DBUG_ENTER("tp_end");
 
   if (!threadpool_started) DBUG_VOID_RETURN;
-
-  stop_timer(&pool_timer);
+  if (timer_thread_started) stop_timer(&pool_timer);
   for (uint i = 0; i < array_elements(all_groups); i++) {
     thread_group_close(&all_groups[i]);
   }

dlenev/percona-server

sql/conn_handler/connection_handler_impl.h
@@ -134,6 +134,8 @@ class Thread_pool_connection_handler : public Connection_handler {
   bool add_connection(Channel_info *channel_info) override;
 
   uint get_max_threads() const override { return threadpool_max_threads; }
+
+  void post_daemonize_init() override { tp_start_timer_thread(); }
 };
 
 #endif  // CONNECTION_HANDLER_IMPL_INCLUDED

sql/threadpool.h
@@ -80,4 +80,7 @@ extern void tp_set_max_threads(uint val);
 extern void tp_set_threadpool_size(uint val) noexcept;
 extern void tp_set_threadpool_stall_limit(uint val) noexcept;
 
+/* Function to start the timer thread */
+extern void tp_start_timer_thread() noexcept;
+
 #endif /* THREADPOOL_INCLUDED */

sql/threadpool_unix.cc
@@ -59,6 +59,8 @@ const char *threadpool_high_prio_mode_names[] = {"transactions", "statements",
 
 /** Indicates that threadpool was initialized*/
 static bool threadpool_started = false;
+/** Indicates that timer thread was started*/
+static bool timer_thread_started = false;
 
 /*
   Define PSI Keys for performance schema.
@@ -603,6 +605,7 @@ static void check_stall(thread_group_t *thread_group) {
 static void start_timer(pool_timer_t *timer) noexcept {
   my_thread_handle thread_id;
   DBUG_ENTER("start_timer");
+  timer_thread_started = true;
   mysql_mutex_init(key_timer_mutex, &timer->mutex, NULL);
   mysql_cond_init(key_timer_cond, &timer->cond);
   timer->shutdown = false;
@@ -610,10 +613,16 @@ static void start_timer(pool_timer_t *timer) noexcept {
   DBUG_VOID_RETURN;
 }
 
+void tp_start_timer_thread() noexcept {
+  pool_timer.tick_interval = threadpool_stall_limit;
+  start_timer(&pool_timer);
+}
+
 static void stop_timer(pool_timer_t *timer) noexcept {
   DBUG_ENTER("stop_timer");
   mysql_mutex_lock(&timer->mutex);
   timer->shutdown = true;
+  timer_thread_started = false;
   mysql_cond_signal(&timer->cond);
   mysql_mutex_unlock(&timer->mutex);
   DBUG_VOID_RETURN;
@@ -1494,8 +1503,6 @@ bool tp_init() {
   mysql_thread_register("threadpool", thread_list, array_elements(thread_list));
 #endif
 
-  pool_timer.tick_interval = threadpool_stall_limit;
-  start_timer(&pool_timer);
   DBUG_RETURN(false);
 }
 
@@ -1503,8 +1510,7 @@ void tp_end() noexcept {
   DBUG_ENTER("tp_end");
 
   if (!threadpool_started) DBUG_VOID_RETURN;
-
-  stop_timer(&pool_timer);
+  if (timer_thread_started) stop_timer(&pool_timer);
   for (uint i = 0; i < array_elements(all_groups); i++) {
     thread_group_close(&all_groups[i]);
   }